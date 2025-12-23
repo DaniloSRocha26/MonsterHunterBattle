da0cf6914d2f38b7a532818dc0c7ecdc775dbc2e
Danilo.Dev <182436555+DaniloSRocha26@users.noreply.github.com>
2025-12-23 08:16:58 -0300

    Adicionando catálogo da Gun Lance

--- dist/data/catalogs/GunLanceCatalog.d.ts
+++ dist/data/catalogs/GunLanceCatalog.d.ts
@@ -1,2 +1,20 @@
-export {};
+export declare const GunLanceList: ({
+    name: string;
+    elemenent: string;
+    source: string;
+    element?: never;
+    status?: never;
+} | {
+    name: string;
+    element: string;
+    source: string;
+    elemenent?: never;
+    status?: never;
+} | {
+    name: string;
+    status: string;
+    source: string;
+    elemenent?: never;
+    element?: never;
+})[];
 //# sourceMappingURL=GunLanceCatalog.d.ts.map
--- dist/data/catalogs/GunLanceCatalog.js
+++ dist/data/catalogs/GunLanceCatalog.js
@@ -1,3 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.GunLanceList = void 0;
+exports.GunLanceList = [{
+        name: "Crimson Rook", elemenent: "Fire", source: "Rathalos"
+    },
+    {
+        name: "Falarmata-of-the-Waves", element: "Water", source: "Uth Duna"
+    },
+    {
+        name: "Fulgurcannon Guardiana", element: "Thunder", source: "Guardian Fulgur Anjanath"
+    },
+    {
+        name: "Precipice Lohamata", element: "Ice", source: "Jin Dahaad"
+    },
+    {
+        name: "Lawful Bors", element: "Dragon", source: "Arkveld"
+    },
+    {
+        name: "Big Slugger", status: "Poison", source: "Gypceros"
+    },
+    {
+        name: "Debilitating Torpor", status: "Paralysis", source: "Lala Barina"
+    },
+    {
+        name: "White Cannon", status: "Blast", source: "Gravios"
+    }
+];
 //# sourceMappingURL=GunLanceCatalog.js.map
