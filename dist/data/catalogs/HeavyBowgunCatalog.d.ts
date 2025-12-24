eff5045b7fbb01503d83e83feef3b17c6e297fcd
Danilo.Dev <182436555+DaniloSRocha26@users.noreply.github.com>
2025-12-24 08:44:22 -0300

    Adição do catálogo da Heavy Bowgun

---
+++ dist/data/catalogs/HeavyBowgunCatalog.d.ts
@@ -0,0 +1,20 @@
+export declare const LightBowGunList: ({
+    name: string;
+    elemenent: string;
+    source: string;
+    element?: never;
+    status?: never;
+} | {
+    name: string;
+    element: string;
+    source: string;
+    elemenent?: never;
+    status?: never;
+} | {
+    name: string;
+    status: string;
+    source: string;
+    elemenent?: never;
+    element?: never;
+})[];
+//# sourceMappingURL=HeavyBowgunCatalog.d.ts.map
---
+++ dist/data/catalogs/HeavyBowgunCatalog.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LightBowGunList = void 0;
+exports.LightBowGunList = [{
+        name: "Esperanza Cannon", elemenent: "Fire", source: "Rathalos"
+    },
+    {
+        name: "Steel Assault", element: "Water", source: "Balahara"
+    },
+    {
+        name: "Gigasbreaker Urbolkule", element: "Thunder", source: "Rey Dau"
+    },
+    {
+        name: "Precipice Malmiam", element: "Ice", source: "Jin Dahaad"
+    },
+    {
+        name: "Aeternal Palamedes", element: "Dragon", source: "Arkveld"
+    },
+    {
+        name: "Ableben Abzug", status: "Poison", source: "Rompopolo"
+    },
+    {
+        name: "Trembling Hels", status: "Paralysis", source: "Lala Barina"
+    },
+    {
+        name: "Blazing Mitran", status: "Blast", source: "Gravios"
+    }
+];
+//# sourceMappingURL=HeavyBowgunCatalog.js.map
