2beb10fd37749b2204da3f60ff8d70e14ed0b18b
Danilo.Dev <182436555+DaniloSRocha26@users.noreply.github.com>
2025-12-24 08:43:37 -0300

    Adição do Catálogo da Heavy Bowgun

---
+++ src/data/catalogs/HeavyBowgunCatalog.ts
@@ -0,0 +1,26 @@
+export const LightBowGunList = [{
+    name: "Esperanza Cannon", elemenent: "Fire", source: "Rathalos"
+},
+{
+    name: "Steel Assault", element: "Water", source: "Balahara"
+},
+{
+    name: "Gigasbreaker Urbolkule", element: "Thunder", source: "Rey Dau"
+},
+{
+    name: "Precipice Malmiam", element: "Ice", source: "Jin Dahaad"
+},
+{
+    name: "Aeternal Palamedes", element: "Dragon", source: "Arkveld"
+},
+{
+    name: "Ableben Abzug", status: "Poison", source: "Rompopolo"
+},
+{
+    name: "Trembling Hels", status: "Paralysis", source: "Lala Barina"
+},
+{
+    name: "Blazing Mitran", status: "Blast", source: "Gravios"
+}
+]
+
