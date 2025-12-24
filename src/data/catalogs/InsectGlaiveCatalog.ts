67b752644aa2fef51b46b53bf19394439b169543
Danilo.Dev <182436555+DaniloSRocha26@users.noreply.github.com>
2025-12-24 10:48:25 -0300

    Adição do Catálogo da Insect Glaive

---
+++ src/data/catalogs/InsectGlaiveCatalog.ts
@@ -0,0 +1,37 @@
+export const InsectGlaiveList = [
+    {
+        name: "Abaddonian Skyspear",
+        element: "Fire",
+        source: "Nu Udra"
+    },
+    {
+        name: "Prinvrilo's Ascension",
+        element: "Water",
+        source: "Xu Wu"
+    },
+    {
+        name: "Fulgur Wingblade",
+        element: "Thunder",
+        source: "Guardian Fulgur Anjanath"
+    },
+    {
+        name: "Glacial Soarstaff",
+        element: "Ice",
+        source: "Jin Dahaad"
+    },
+    {
+        name: "Stalwart Skyreign",
+        element: "Dragon",
+        source: "Arkveld"
+    },
+    {
+        name: "Venomwing Glaive",
+        status: "Poison",
+        source: "Nerscylla"
+    },
+    {
+        name: "Paralyzing Skydancer",
+        status: "Paralysis",
+        source: "Lala Barina"
+    },
+];
