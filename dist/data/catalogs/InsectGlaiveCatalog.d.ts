b79da7e74856c9044796ded8c9a847da68f9d873
Danilo.Dev <182436555+DaniloSRocha26@users.noreply.github.com>
2025-12-24 10:48:56 -0300

    Adição do catálogo da Insect Glaive

---
+++ dist/data/catalogs/InsectGlaiveCatalog.d.ts
@@ -0,0 +1,12 @@
+export declare const InsectGlaiveList: ({
+    name: string;
+    element: string;
+    source: string;
+    status?: never;
+} | {
+    name: string;
+    status: string;
+    source: string;
+    element?: never;
+})[];
+//# sourceMappingURL=InsectGlaiveCatalog.d.ts.map
